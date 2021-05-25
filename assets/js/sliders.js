7446a000bdb78e0c478f6e9a5443ffc42bf44477
adamNanista <adamko.nanista1111@gmail.com>
2021-05-25 10:28:37 +0200

    Add files via upload

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,33 +1,41 @@
 
 (function () {
 
-	new Splide( '.partners', {
-		type: 'loop', 
-		perPage: 6, 
-		perMove: 1, 
-		pagination: false, 
-		breakpoints: {
-			1024: {
-				perPage: 3,
+	
+	if (document.querySelector('.partners')) {
+		new Splide( '.partners', {
+			type: 'loop', 
+			perPage: 6, 
+			perMove: 1, 
+			autoplay: true, 
+			interval: 3000,
+			pagination: false, 
+			breakpoints: {
+				1024: {
+					perPage: 3,
+				},
+				767: {
+					perPage: 2,
+				}, 
 			},
-			767: {
-				perPage: 2,
-			}, 
-		},
-	} ).mount();
-	new Splide( '.testimonials', {
-		type: 'loop', 
-		perPage: 3, 
-		perMove: 1, 
-		pagination: false, 
-		breakpoints: {
-			1024: {
-				perPage: 2,
+		} ).mount();
+	}
+	
+	if (document.querySelector('.testimonials')) {
+		new Splide( '.testimonials', {
+			type: 'loop', 
+			perPage: 3, 
+			perMove: 1, 
+			pagination: false, 
+			breakpoints: {
+				1024: {
+					perPage: 2,
+				},
+				767: {
+					perPage: 1,
+				}, 
 			},
-			767: {
-				perPage: 1,
-			}, 
-		},
-	} ).mount();
+		} ).mount();
+	}
 
 })();
